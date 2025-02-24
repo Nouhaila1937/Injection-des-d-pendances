9274b5783ca449c5f10b8c9f21df57657f6149bb
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-02-24 11:26:40 +0000

    implementation version spring xml

--- src/main/java/pres/presentation_spring_xml.java
+++ src/main/java/pres/presentation_spring_xml.java
@@ -1,12 +1,15 @@
 package pres;
 
+import metier.IMetier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class presentation_spring_xml {
     public static void main(String[] args) {
+        //injection on utilisant spring version xml
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-
+        IMetier metier = (IMetier) context.getBean("metier");
+        System.out.println("RES= "+metier.calcul());
 
     }
 }
